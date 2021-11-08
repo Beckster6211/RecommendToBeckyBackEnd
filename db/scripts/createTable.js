579f2cf6758905c914e342d49568b63216e2c9b7
= <beckyljones151192@gmail.com>
2021-11-08 12:34:46 +0000

    added defaults when creating table for null values

--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -3,9 +3,9 @@ let { query } = require("../index");
 let sqlStatement = `CREATE TABLE IF NOT EXISTS foodTable(
     id SERIAL PRIMARY KEY, 
     food TEXT,
-    recommendedBy TEXT,
-    beckyOpinion TEXT,
-    isDone BOOLEAN
+    recommendedBy TEXT DEFAULT '',
+    beckyOpinion TEXT DEFAULT '',
+    isDone BOOLEAN DEFAULT false
 );`;
 
 async function createTable() {
