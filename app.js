f5dbd96c69ae10b3e59dd54f345a47ade5cc642c
= <beckyljones151192@gmail.com>
2021-12-01 11:54:57 +0000

    installed cookie parser to try and stop post error when logged in

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var cookieParser = require("cookie-parser");
+// var bodyParser = require('body-parser')
 const port = 3001;
 
 const foodRouter = require("./routes/food");
@@ -13,6 +15,7 @@ app.get("/", (req, res) => {
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/food", foodRouter);
 app.use("/visit", visitRouter);
