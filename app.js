8ac5df5076f580b1106711b95e8dcf085ef5edf6
= <beckyljones151192@gmail.com>
2021-12-03 14:36:56 +0000

    changed from localhost to heroku

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use("/visit", visitRouter);
 app.use("/stay", stayRouter);
 app.use("/read", readRouter);
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`);
+// });
 
-// app.listen(process.env.PORT || 3000);
-// module.exports = app;
+app.listen(process.env.PORT || 3000);
+module.exports = app;
